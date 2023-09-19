c51c01acb18974aa75dff2d1acf8abfcab5918a0
fileofish <oleg.b9113@gmail.com>
2023-09-19 22:48:07 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Здавствуйте!</h1>
+    <p>
+      У нашей команды возникли трудности с деплоем на Netlify.
+      Деплой был перенесен на Vercel.
+    </p>
+    <p>
+      Для проверки нашей работы просим перейти по сслыке
+      <a href="https://e-commerce-application-eight.vercel.app/" class="link"
+        >https://e-commerce-application-eight.vercel.app/</a
+      >
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+p {
+  font-size: 18px;
+}
+.link {
+  font-size: 24px;
+}
+</style>
